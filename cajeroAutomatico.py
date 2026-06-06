7a8265abe24cc5b4e8687589e84fed12011afb01
Osvaldo zapata <osvaldozapatapuerta@gmail.com>
2026-06-05 23:31:07 -0500

    Menu

--- cajeroAutomatico.py
+++ cajeroAutomatico.py
@@ -14,3 +14,15 @@ historial3 = "Sin movimiento"
 
 #variable del control del menun
 opcion = 0
+
+while opcion != 6:
+    print("MENU ----CAJERO AUTOMATICO OZLAURED")
+    print("1. Consultar saldo")
+    print("2. Depositar dinero")
+    print("3. Retirar dinero")
+    print("4. Cambiar contraseña")
+    print("5. Ver Movimientos")
+    print("6. Cerrar")
+    
+opcion = int(input("Seleccione una opcion:   ")) 
+
