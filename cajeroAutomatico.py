640e6edb2dac9ac9490f4ace3311b768fd375337
Osvaldo zapata <osvaldozapatapuerta@gmail.com>
2026-06-05 23:51:12 -0500

    Condicones completadas

--- cajeroAutomatico.py
+++ cajeroAutomatico.py
@@ -2,8 +2,8 @@
 usuario = (input("Ingrese su usuario  "))
 usuario = usuario
 
-contraseña = int(input("Ingrese su contraseña  "))
-contraseña = contraseña
+clave = int(input("Ingrese su contraseña  "))
+clave = clave
 
 saldo = 0
 
@@ -12,7 +12,7 @@ historial1 = "Sin movimiento"
 historial2 = "Sin movimiento"
 historial3 = "Sin movimiento"
 
-#variable del control del menun
+#control del menu
 opcion = 0
 
 while opcion != 6:
@@ -26,3 +26,59 @@ while opcion != 6:
     
 opcion = int(input("Seleccione una opcion:   ")) 
 
+if opcion == 1:
+    print(f"Su saldo es de: ${saldo}")
+    
+    historial1 = historial2
+    historial2 = historial3
+    historial3 = "consulta de saldo"
+    
+elif opcion == 2:
+    ingreso = int(input("Ingrese el valor a depositar:   "))
+    saldo = saldo + ingreso
+    print(f"Deposito realizado. Su nuevo saldo es de: ${saldo}")
+    
+    historial1 = historial2
+    historial2 = historial3
+    historial3 = f"Deposito de ${ingreso}"
+    
+elif opcion == 3:
+    retiro = int(input("Ingrese el valor a retirar:   "))
+    
+    if retiro > saldo:
+        print("Saldo insuficiente")
+    else:
+        saldo = saldo - retiro  
+        print(f"Retiro Exitoso. saldo restante es de ${saldo}")
+        
+        historial1 = historial2
+        historial2 = historial3
+        historial3 = f"Retiro de ${retiro}"
+
+elif opcion == 4:
+        claveActual = int(input("Ingrese su contraseña actual:   "))
+        
+        if claveActual == clave:
+            nuevaClave = int(input("Ingrese su nueva contraseña:   "))
+            clave = nuevaClave
+            print("Cambio de contraseña exitoso")
+            
+            historial1 = historial2
+            historial2 = historial3
+            historial3 = "Cambio de contraseña"
+            
+        else:
+            print("contraseña incorrecta")
+
+elif opcion == 5:
+    print("--- HISTORIAL ---")
+    print( historial1 )
+    print( historial2 )
+    print( historial3 )
+    
+elif opcion == 6:
+    print("Gracias")
+    
+else:
+    print("Opcion no valida.")        
+            
