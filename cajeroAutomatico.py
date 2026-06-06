fa1026deb0be9b11189b1e5b41a0f90a03d09695
Osvaldo zapata <osvaldozapatapuerta@gmail.com>
2026-06-05 22:49:36 -0500

    Primeras condiciones

---
+++ cajeroAutomatico.py
@@ -0,0 +1,29 @@
+#solicitud de datos del usuario
+usuario = (input("Ingrese su nombre  "))
+usuario = usuario
+
+contraseña = int(input("Ingrese su contraseña  "))
+contraseña = contraseña
+
+saldo = None
+
+#validacion de accion que quiere hacer
+accion = (input("menu : consultarsaldo, depostardinero, retirardinero  "))
+
+if accion == "consultarSaldo":
+    print("saldo")
+elif accion == "depostardinero":
+    ingreso = int(input("Ingrese la cantidad  "))
+    saldo = ingreso
+    print("saldo")
+    
+
+
+
+
+
+
+
+
+
+
