d886931ea088fe6b30c7c49167ed3616011ce4eb
Osvaldo zapata <osvaldozapatapuerta@gmail.com>
2026-06-07 08:34:56 -0500

    Creacion de usuario y contraseña registro

--- cajeroAutomatico.py
+++ cajeroAutomatico.py
@@ -1,6 +1,41 @@
+import sys
+#Mensaje inicial del cajero
+print ("-------Bienvenid@ a OZLAURED, tu banco de confianza------")
+print("------- Registro de nueva cuenta -------")
+
+#Creación de usuario y contraseña
+usuario_registrado = input("Cree su nombre de usuario: ")
+clave_registrada = int(input("Cree su contraseña (numérica): "))
+print("Usuario creado con éxito.\n")
+
+#Solicitud de datos del usuario
+intentos = 3
+login_exitoso = False
+
+print("------- INICIO DE SESIÓN -------")
+while intentos > 0:
+    usuario_ingresado = input("Ingrese su usuario: ")
+    clave_ingresada = int(input("Ingrese su contraseña: "))
+
+    if usuario_ingresado == usuario_registrado and clave_ingresada == clave_registrada:
+        print("\n¡Bienvenido al sistema!")
+        login_exitoso = True
+        break  #Rompe el bucle si los datos son correctos
+    else:
+        intentos -= 1
+        print(f"Usuario o contraseña incorrectos. Intentos restantes: {intentos}")
+        if intentos > 0:
+            print("Inténtelo de nuevo.\n")
+
+#Cerrar el programa al tercer intento fallido
+if not login_exitoso:
+    print("Ha superado el límite de intentos permitidos. Sistema bloqueado.")
+    sys.exit()  #Cierra el programa por completo
+
+
 #solicitud de datos del usuario
 usuario = (input("Ingrese su usuario  "))
-clave = int(input("Ingrese su contraseña  "))
+clave = clave_registrada #La clave actual será la que registró
 
 saldo = 0
 
